f0ee59e6d055e09e53376bdb016e865d4f43d20e
Chen YiJie <coaixy@qq.com>
2024-01-02 20:50:49 +0800

    Update resume-data.tsx

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -50,12 +50,12 @@ export const RESUME_DATA = {
     ],
   },
     education: [
-    // {
-    //     school: "Wrocław University of Technology",
-    //     degree: "Bachelor's Degree in Control systems engineering and Robotics",
-    //     start: "2007",
-    //     end: "2010",
-    // },
+    {
+        school: "",
+        degree: "",
+        start: "",
+        end: "",
+    },
   ],
     work: [
     {
